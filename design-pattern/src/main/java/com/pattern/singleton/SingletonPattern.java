1edb66fef4e9d112920997dcba879155bc36877b
Jayesh <jaiswaljayesh46@gmail.com>
2022-04-19 19:28:37 +0530

    sigleton and factory

---
+++ design-pattern/src/main/java/com/pattern/singleton/SingletonPattern.java
@@ -0,0 +1,21 @@
+package com.pattern.singleton;
+
+public class SingletonPattern {
+
+    //create an object of SingleObject
+    private static SingletonPattern instance = new SingletonPattern();
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private SingletonPattern(){}
+
+    //Get the only object available
+    public static SingletonPattern getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+
+}
